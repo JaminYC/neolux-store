1716fe9704f9ff626cfe5d793134477c3e437c15
JaminYC <a20212422@pucp.edu.pe>
2025-01-05 14:34:16 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bienvenido a NeoLux</h1>
+      <p>Tu plataforma para grabados y cortes láser.</p>
+    </div>
+  );
+};
+
+export default App;
